2ed5599d6893055831d229bfe8d9e5350903e5e9
Gem Eliezer <gemeliezerespiritu@gmail.com>
2021-12-25 21:12:45 +0800

    payment form pop up

--- src/Components/Landing/PlanPopUp.js
+++ src/Components/Landing/PlanPopUp.js
@@ -2,7 +2,14 @@ import { useState } from "react"
 import { useEffect } from "react"
 import Input from "../Assets/Input"
 
+import { firebase } from "../../firebase"
+import { usePopUp } from "../Contexts/PopUpContext"
+
+const auth = firebase.auth()
+
 const PlanPopUp = () => {
+    const { setPopUpContent } = usePopUp()
+
     const [email, setEmail] = useState({name: "email", label: "Email", type: "email", value: "", isValid: false, isRequired: true})
     const [cardNumber, setCardNumber] = useState({name: "cardNumber", label: "Card Number", type: "text", value: "", isValid: false, isRequired: true})
     const [expirationDate, setExpirationDate] = useState({name: "expirationDate", label: "MM / YY", type: "text", value: "", isValid: false, isRequired: true})
@@ -44,8 +51,12 @@ const PlanPopUp = () => {
         }
     }
 
+    useEffect(() => {
+        setEmail({...email, value: auth.currentUser.email})
+    }, [auth])
+
     return (
-        <div className="column dark color-inherit" style={{
+        <div className="pos-relative dark color-inherit" style={{
             maxWidth: "600px",
             width: "100%",
             borderBottomLeftRadius: "10px",
@@ -53,65 +64,76 @@ const PlanPopUp = () => {
             zIndex: "1",
             backgroundColor: "var(--indigo)",
             maxHeight: "100%",
-            overflow: "auto",
             borderTopLeftRadius: "10px",
             borderTopRightRadius: "10px",
-            padding: "32px",
             color: "white"
         }}>
-            <div className="column align-center">
-                <p className="uppercase">
-                    Basic
-                </p>
-                <p className="fs-80 margin-top-10">
-                    $9
-                </p>
-                <p>
-                    per month, billed monthly
-                </p>
+            <div className="img-32 icon cursor-pointer" style={{
+                backgroundColor: "var(--bg-color-2)",
+                position: "absolute",
+                top: "0",
+                right: "0",
+                marginRight: "8px",
+                marginTop: "8px"
+            }} onClick={() => setPopUpContent()}>
+                <i className="fas fa-times"></i>
             </div>
-            <form className="column gap-10 margin-top-20" onChange={(e) => {
-                formHander(e.target)
+            <div className="column" style={{
+                width: "100%",
+                height: "100%",
+                overflow: "auto",
+                padding: "32px",
             }}>
-                <input className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui space-between border-radius-200 row" style={{
-                    backgroundColor: "var(--bg-comp-color-2)",
-                    color: "var(--text-comp-color-2)",
-                }} value="gemeliezerespiritu@gmail.com"/>
-                <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui border-radius-200 row" style={{
-                    backgroundColor: "var(--bg-comp-color-2)",
-                    color: "var(--text-comp-color-2)",
+                <div className="column align-center">
+                    <p className="uppercase">
+                        Basic
+                    </p>
+                    <p className="fs-80 margin-top-10">
+                        $9
+                    </p>
+                    <p>
+                        per month, billed monthly
+                    </p>
+                </div>
+                <form className="column gap-10 margin-top-20" onChange={(e) => {
+                    formHander(e.target)
                 }}>
-                    <input placeholder="Card Number" style={{
-                        width: "50%",
-                        justifySelf: "start"
-                    }}/>
-                    <div className="row gap-6" style={{
-                        whiteSpace: "nowrap",
-                        width: "50%",
-                        justifySelf: "end"
+                    <input className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui space-between border-radius-200 row" style={{
+                        backgroundColor: "var(--bg-comp-color-2)",
+                        color: "var(--text-comp-color-2)",
+                    }} value={email.value}/>
+                    <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui border-radius-200 row" style={{
+                        backgroundColor: "var(--bg-comp-color-2)",
+                        color: "var(--text-comp-color-2)",
                     }}>
-                        {/* <input type="date" name="expirationDate" placeholder="MM / YY" style={{
-                            width: "100%",
-                        }} value={
-                            expirationDate.value
-                        }/> */}
-                        <input type="email" name="expirationDate" placeholder="MM / YY" style={{
-                            width: "100%",
-                        }} value={
-                            expirationDate.value
-                        }/>
-                        <input placeholder="CVC" style={{
-                            width: "100%",
+                        <input placeholder="Card Number" style={{
+                            width: "50%",
+                            justifySelf: "start"
                         }}/>
+                        <div className="row gap-6" style={{
+                            whiteSpace: "nowrap",
+                            width: "50%",
+                            justifySelf: "end"
+                        }}>
+                            <input type="email" name="expirationDate" placeholder="MM / YY" style={{
+                                width: "100%",
+                            }} value={
+                                expirationDate.value
+                            }/>
+                            <input placeholder="CVC" style={{
+                                width: "100%",
+                            }}/>
+                        </div>
                     </div>
-                </div>
-                <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui justify-center border-radius-200 row" style={{
-                    backgroundColor: "var(--bg-color-2)",
-                    color: "white"
-                }}>
-                    Subscribe Now
-                </div>
-            </form>
+                    <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui justify-center border-radius-200 row" style={{
+                        backgroundColor: "var(--bg-color-2)",
+                        color: "white"
+                    }}>
+                        Subscribe Now
+                    </div>
+                </form>
+                <p className="fs-10 margin-top-20 fw-700">You will be immediately charged $19 for a one month access. Your plan will automatically renew unless cancelled before the renewal date. You are also agreeing to our Terms of Service and our Privacy Policy.</p>
+            </div>
         </div>
     )
 }
