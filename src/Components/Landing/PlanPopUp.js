63262971f6a927e989758d85c1792caf9ce2ffc7
Gem Eliezer <gemeliezerespiritu@gmail.com>
2021-12-25 18:56:17 +0800

    payment form

--- src/Components/Landing/PlanPopUp.js
+++ src/Components/Landing/PlanPopUp.js
@@ -1,11 +1,49 @@
 import { useState } from "react"
-import { useEffect } from "react/cjs/react.development"
+import { useEffect } from "react"
 import Input from "../Assets/Input"
 
 const PlanPopUp = () => {
     const [email, setEmail] = useState({name: "email", label: "Email", type: "email", value: "", isValid: false, isRequired: true})
     const [cardNumber, setCardNumber] = useState({name: "cardNumber", label: "Card Number", type: "text", value: "", isValid: false, isRequired: true})
-    const [expirationDate, setExpirationDate] = useState({name: "expirationDate", label: "Expiration Date", type: "text", value: "", isValid: false, isRequired: true})
+    const [expirationDate, setExpirationDate] = useState({name: "expirationDate", label: "MM / YY", type: "text", value: "", isValid: false, isRequired: true})
+    const [CVC, setCVC] = useState({name: "CVC", label: "CVC", type: "number", value: "", isValid: false, isRequired: true})
+
+    const formHander = (e) => {
+        switch (e.name) {
+            case "email":
+                setEmail(e.value)
+            case "cardNumber":
+               
+            case "expirationDate":
+                if (e.value.length > 4) {
+                    
+                }
+                else {
+                    if (e.value.slice(0, 2) > 12) {
+                    }
+                    else {
+                        if(e.value.length >= 3) {
+                            setExpirationDate({
+                                ...expirationDate,
+                                value: e.value.slice(0, 2) + " / " + e.value.slice(2, 4)
+                            })
+                        }
+                        if (e.value[0] == 1 || e.value[0] == 0) {
+                            setExpirationDate({...expirationDate, value: e.value})
+                        }
+                        else if (e.value[0] >= 2) {
+                            setExpirationDate({...expirationDate, value: "0" + e.value})
+                        }
+                        else if (!e.value[0]){
+                            setExpirationDate({...expirationDate, value: e.value})
+                        }
+                    }
+                }
+                break
+            default:
+                break
+        }
+    }
 
     return (
         <div className="column dark color-inherit" style={{
@@ -33,7 +71,9 @@ const PlanPopUp = () => {
                     per month, billed monthly
                 </p>
             </div>
-            <form className="column gap-10 margin-top-20">
+            <form className="column gap-10 margin-top-20" onChange={(e) => {
+                formHander(e.target)
+            }}>
                 <input className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui space-between border-radius-200 row" style={{
                     backgroundColor: "var(--bg-comp-color-2)",
                     color: "var(--text-comp-color-2)",
@@ -51,9 +91,9 @@ const PlanPopUp = () => {
                         width: "50%",
                         justifySelf: "end"
                     }}>
-                        <input placeholder="MM / YY" style={{
+                        <input type="email" name="expirationDate" placeholder="MM / YY" style={{
                             width: "100%",
-                        }}/>
+                        }} value={expirationDate.value}/>
                         <input placeholder="CVC" style={{
                             width: "100%",
                         }}/>
