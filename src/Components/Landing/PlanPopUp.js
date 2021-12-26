6bec4241869c33d2e6b48470c4f214bc171816c5
Gem Eliezer <gemeliezerespiritu@gmail.com>
2021-12-26 23:43:42 +0800

    payment form

--- src/Components/Landing/PlanPopUp.js
+++ src/Components/Landing/PlanPopUp.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useEffect } from "react"
 import Input from "../Assets/Input"
 import CreditCardInput from 'react-credit-card-input'
@@ -12,6 +12,9 @@ const auth = firebase.auth()
 const PlanPopUp = () => {
     const { setPopUpContent } = usePopUp()
     const { isLoggedIn } = useAuth()
+    const paypal = useRef()
+
+    const [checkout, setCheckout] = useState()
 
     const [email, setEmail] = useState({name: "email", label: "Email", type: "email", value: "", isValid: false, isRequired: true})
     const [cardNumber, setCardNumber] = useState({name: "cardNumber", label: "Card Number", type: "text", value: "", isValid: false, isRequired: true})
@@ -19,10 +22,15 @@ const PlanPopUp = () => {
     const [CVC, setCVC] = useState({name: "CVC", label: "CVC", type: "number", value: "", isValid: false, isRequired: true})
 
     const formHander = (e) => {
-        console.log(e)
         switch (e.name) {
             case "expirationDate":
                 e.value.length <= 4 && setExpirationDate({...expirationDate, value: e.value})
+                break
+            case "cardNumber":
+                e.value.length <= 4 && setCardNumber({...cardNumber, value: e.value})
+                break
+            case "CVC":
+                e.value.length <= 4 && setCVC({...CVC, value: e.value})
                 break
         }
     }
@@ -30,6 +38,35 @@ const PlanPopUp = () => {
     useEffect(() => {
         isLoggedIn && setEmail({...email, value: auth.currentUser.email})
     }, [auth])
+
+    useEffect(() => {
+        window.paypal.Buttons({
+            
+            createOrder: (data, actions, err) => {
+                console.log(data, actions, err)
+                return actions.order.create({
+                intent: "CAPTURE",
+                purchase_units: [
+                    {
+                    description: "Cool looking table",
+                    amount: {
+                        currency_code: "USD",
+                        value: 9,
+                    },
+                    },
+                ],
+                })
+            },
+            onApprove: async (data, actions) => {
+                // const order = await actions.order.capture();
+                // console.log(order);
+                console.log(data, actions)
+            },
+            onError: (err) => {
+                console.log(err);
+            },
+        }).render(paypal.current)
+    }, [])
 
     useEffect(() => {
         console.log(isLoggedIn)
@@ -98,29 +135,21 @@ const PlanPopUp = () => {
                                 width: "80px",
                             }}/>
                     </div>
-                    {/* <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui border-radius-200 row" style={{
-                        backgroundColor: "var(--bg-comp-color-2)",
-                        color: "var(--text-comp-color-2)",
-                    }}>
-                        <input placeholder="Card Number" style={{
-                            minWidth: "100%",
-                            justifySelf: "start"
-                        }}/>
-                            <input type="email" name="expirationDate" placeholder="MM / YY" style={{
-                                width: "80px",
-                            }} value={
-                                expirationDate.value
-                            }/>
-                            <input placeholder="CVC" style={{
-                                width: "80px",
-                            }}/>
-                    </div> */}
-                    <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui justify-center border-radius-200 row" style={{
+                    <div ref={paypal}>
+
+                    </div>
+                    {/* <div ref={paypal} className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui justify-center border-radius-200 row cursor-pointer" style={{
                         backgroundColor: "var(--bg-color-2)",
                         color: "white"
-                    }}>
+                    }} onClick={() => console.log("ASD")}>
                         Subscribe Now
-                    </div>
+                    </div> */}
+                    {/* <div className="padding-x-25 padding-y-10 color-inherit fw-900 ff-ui justify-center border-radius-200 row cursor-pointer" style={{
+                        backgroundColor: "var(--bg-color-2)",
+                        color: "white"
+                    }} onClick={() => console.log("ASD")}>
+                        Subscribe Now
+                    </div> */}
                 </form>
                 <p className="fs-10 margin-top-20 fw-700">You will be immediately charged $19 for a one month access. Your plan will automatically renew unless cancelled before the renewal date. You are also agreeing to our Terms of Service and our Privacy Policy.</p>
             </div>
